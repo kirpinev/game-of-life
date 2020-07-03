0f5af0f734bf401028777066194cba43ed1317af
Igor Kirpinev <kirpinev@yahoo.com>
2020-07-03 14:52:49 +0300

    Добавил uikit

---
+++ src/uikit/Title.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: white;
+  margin: auto;
+  text-align: center;
+  width: 50%;
+  margin-top: 10px;
+`;
+
+export default Title;
